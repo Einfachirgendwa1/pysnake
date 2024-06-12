b2d140d829b236870ce41d821bb13de9312ddc6e
Einfachirgendwa1 <faris@karge.berlin>
2024-06-12 15:58:48 +0200

    Einfaches pygame script

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hi!")
+# pyright:strict
+import pygame
+
+WIDTH, HEIGHT = 800, 800
+
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type is pygame.QUIT:
+            running = False
