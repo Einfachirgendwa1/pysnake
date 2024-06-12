e19660e2f631670bbb2ffcb4a03c30ef367d4d0b
Einfachirgendwa1 <faris@karge.berlin>
2024-06-12 16:13:33 +0200

    Position class und random_position funktion hinzugefügt

--- main.py
+++ main.py
@@ -1,10 +1,38 @@
 # pyright:strict
+import random
+from typing import Tuple
+
 import pygame
 
-WIDTH, HEIGHT = 800, 800
+# Size vom erstellten Fenster
+SCREEN = 800, 800
+GRID_SIZE = 25, 25
+
+BLOCK_SIZE = (SCREEN[0] // GRID_SIZE[0], SCREEN[1] // GRID_SIZE[1])
+
+
+# Snake ist ja in Kästchen aufgeteilt, und das ist quasi eine Kästchenposition
+class Position:
+    def __init__(self, x: int, y: int) -> None:
+        assert x <= GRID_SIZE[0]
+        assert y <= GRID_SIZE[1]
+        self.x = x
+        self.y = y
+
+    def to_pygame_pos(self) -> Tuple[int, int]:
+        return (self.x * BLOCK_SIZE[0], self.y * BLOCK_SIZE[1])
+
+
+# Generiert eine zufällige Kästchenposition
+def random_position() -> Position:
+    return Position(random.randint(0, GRID_SIZE[0]), random.randint(0, GRID_SIZE[1]))
+
+
+# Position vom Apfel
+apple_position = random_position()
 
 pygame.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen = pygame.display.set_mode(SCREEN)
 
 running = True
 while running:
