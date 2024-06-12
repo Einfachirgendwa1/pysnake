13b9ae1e4a80ed7ec75d138aed20ac7cb10cdd10
Einfachirgendwa1 <faris@karge.berlin>
2024-06-12 16:31:54 +0200

    Einfache (verbuggte) Renderfunktion geschrieben

--- main.py
+++ main.py
@@ -1,28 +1,40 @@
 # pyright:strict
 import random
-from typing import Tuple
+from typing import Optional, Tuple
 
 import pygame
 
-# HALLLOOOOOOOOOO
-# HI
 # Size vom erstellten Fenster
 SCREEN = 800, 800
 GRID_SIZE = 25, 25
 
 BLOCK_SIZE = (SCREEN[0] // GRID_SIZE[0], SCREEN[1] // GRID_SIZE[1])
 
+pygame.init()
+screen = pygame.display.set_mode(SCREEN)
+
 
 # Snake ist ja in Kästchen aufgeteilt, und das ist quasi eine Kästchenposition
 class Position:
-    def __init__(self, x: int, y: int) -> None:
+    def __init__(
+        self, x: int, y: int, color: Optional[Tuple[int, int, int]] = None
+    ) -> None:
         assert x <= GRID_SIZE[0]
         assert y <= GRID_SIZE[1]
         self.x = x
         self.y = y
+        self.color = color
 
     def to_pygame_pos(self) -> Tuple[int, int]:
         return (self.x * BLOCK_SIZE[0], self.y * BLOCK_SIZE[1])
+
+    def render(self):
+        assert self.color != None
+        start = self.to_pygame_pos()
+        end = list(self.to_pygame_pos())
+        end[0] += BLOCK_SIZE[0]
+        end[1] += BLOCK_SIZE[1]
+        pygame.draw.rect(screen, self.color, (start, end))
 
 
 # Generiert eine zufällige Kästchenposition
@@ -31,13 +43,19 @@ def random_position() -> Position:
 
 
 # Position vom Apfel
-apple_position = random_position()
+apple = random_position()
+apple.color = (255, 0, 0)
 
-pygame.init()
-screen = pygame.display.set_mode(SCREEN)
+
+def render():
+    apple.render()
+
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type is pygame.QUIT:
             running = False
+
+    render()
+    pygame.display.flip()
